9a102f6e42f047f358bc7304599580e3343951d1
Francisco Tarriño <fjtarfer27@insgabrielamistral.cat>
2026-04-29 19:35:39 +0200

    po

--- backend/src/main/java/com/imperiumfitness/config/CorsConfig.java
+++ backend/src/main/java/com/imperiumfitness/config/CorsConfig.java
@@ -11,7 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")          // Aplica CORS a todos tus endpoints de API
                 .allowedOrigins(
-                        "http://localhost:5500",    // El puerto donde sirves el HTML en desarrollo
+                        "http://localhost:5500", 
+                        "http://localhost:5501",   // El puerto donde sirves el HTML en desarrollo
                         "http://127.0.0.1:5500",
                         "http://localhost:8080",    // Por si acaso pruebas la web en otro puerto
                         "http://localhost:3000",    // Típico de React/Node, por si acaso
