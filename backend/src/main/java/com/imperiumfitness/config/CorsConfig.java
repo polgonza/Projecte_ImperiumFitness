b92f0a4f4143e0187ef40eae5e2d3b841c28d07a
Francisco Tarriño <fjtarfer27@insgabrielamistral.cat>
2026-04-29 17:27:36 +0200

    el primero del dia señooreee

--- backend/src/main/java/com/imperiumfitness/config/CorsConfig.java
+++ backend/src/main/java/com/imperiumfitness/config/CorsConfig.java
@@ -15,6 +15,11 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://127.0.0.1:5500",
                         "http://localhost:8080",    // Por si acaso pruebas la web en otro puerto
                         "http://localhost:3000",    // Típico de React/Node, por si acaso
+                        "http://172.21.90.3",           // ← servidor institut
+                        "http://172.21.90.3:5500",      // ← servidor institut amb port
+                        "http://10.147.17.250",         // ← ZeroTier
+                        "http://10.147.17.250:5500",    // ← ZeroTier amb port
+                        "http://10.147.17.250:8086",    // ← ZeroTier backend
                         "capacitor://localhost",    // Para Android (WebView nativo)
                         "http://localhost"          // Para emuladores Android antiguos
                         // Añade aquí la IP del servidor de producción del frontend si la tiene
